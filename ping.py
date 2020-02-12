e80a550a4707e4e68d45dd8420fc46a0b4cf4087
Glory Obielodan <gloryobielodan@gmail.com>
2020-02-12 00:49:05 -0600

    complete script for pinging url

--- ping.py
+++ ping.py
@@ -3,3 +3,10 @@ import requests
 url = 'http://34.205.48.205:3000/'
 
 res = requests.get(url)
+
+if res.status_code != 200:
+    print("ERROR: There's been a problem with the ping response. Response is", res.status_code)
+elif res.text == "":
+    print("ERROR: The response body is empty")
+else:
+    print("Successful Ping. Response is", res.status_code)
